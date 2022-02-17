6715050c87e2717b37825db47bc13be9a7283f3c
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-18 02:32:49 +0900

    Feat: Encrypt password with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // REGISTER
 router.post('/register', async (req, res) => {
-	const newUser = new User({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password,
-	});
-
 	try {
+		// bcrypt 라이브러리를 통해 해시된 패스워드 생성
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashedPassword,
+		});
+
 		const user = await newUser.save();
 		res.status(200).json(user);
 	} catch (err) {

5d107d7014f0cfe67c7003294c57ed349385a696
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-17 23:34:59 +0900

    Feat: mongoose, dotenv 셋업

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('Connected to MongoDB.'))
+	.catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json()); // request body parsing 용도
 
 app.listen(4000, () => {
 	console.log('Backend server is running.');

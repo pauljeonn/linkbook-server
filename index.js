74ae0bb3a3bdd678ef9f0da3f21cdebefceff161
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-27 04:34:07 +0900

    Feat: fileRoute 생성 (multer & AWS S3)

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const multer = require('multer');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const fileRoute = require('./routes/files');
 
 dotenv.config();
 
@@ -19,19 +19,11 @@ mongoose
 app.use(cors());
 app.use(express.json()); // request body parsing 용도
 
-const upload = multer({ dest: 'uploads/' });
-app.post('/api/upload', upload.single('file'), async (req, res) => {
-	try {
-		return res.status(200).json('File uploaded successfully.');
-	} catch (err) {
-		console.log(err);
-	}
-});
-
 // Routes
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/files', fileRoute);
 
 app.listen(4000, () => {
 	console.log('Backend server is running.');
---
+++ routes/files.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+
+// AWS 설정
+aws.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: 'ap-northeast-2',
+});
+
+// 업로드 모듈
+const upload = multer({
+	storage: multerS3({
+		s3: new aws.S3(),
+		bucket: 'linkbook',
+		key(req, file, cb) {
+			cb(null, Date.now() + file.originalname);
+		},
+	}),
+	limits: { fileSize: 10 * 1024 * 1024 }, // 파일 용량 제한
+});
+
+// UPLOAD FILE
+router.post('/upload', upload.single('file'), async (req, res) => {
+	try {
+		let imgURL = { url: req.file.location };
+		return res.status(200).json(imgURL);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+});
+
+module.exports = router;

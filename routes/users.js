d5b9f5a611b6e7cf622ff8b224b1333eb9eba5b1
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-03-01 12:30:13 +0900

    Feat: FOLLOW & UNFOLLOW USER 메서드 추가

--- routes/users.js
+++ routes/users.js
@@ -27,14 +27,39 @@ router.get('/friends/:id', async (req, res) => {
 	}
 });
 
-// GET RECOMMENDED FRIENDS
-router.get('/recommended', async (req, res) => {
+// FOLLOW USER
+router.put('/:id/follow', async (req, res) => {
 	try {
-		const all = await User.find({}).exec();
-
-		res.status(200).json(all);
+		const user = await User.findById(req.params.id);
+		const targetUser = await User.findById(req.body.userId);
+		if (!user.following.includes(req.body.userId)) {
+			await user.updateOne({ $push: { following: req.body.userId } });
+			await targetUser.updateOne({ $push: { followers: req.params.id } });
+			res.status(200).json(user);
+		} else {
+			res.status(403).json('이미 팔로잉하는 유저입니다.');
+		}
 	} catch (err) {
 		res.status(500).json(err);
+	}
+});
+
+// UNFOLLOW USER
+router.put('/:id/unfollow', async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const targetUser = await User.findById(req.body.userId);
+			if (user.following.includes(req.body.userId)) {
+				await user.updateOne({ $pull: { following: req.body.userId } });
+				await targetUser.updateOne({ $pull: { followers: req.params.id } });
+				res.status(200).json(user);
+			} else {
+				res.status(403).json('팔로우하지 않은 유저입니다.');
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
 	}
 });
 
